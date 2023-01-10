599f38fd4623642f7078289976fbdbf1e15b0957
Riccardo Camozzi <camozzi.riccardo06@gmail.com>
2023-01-10 15:08:37 +0100

    Salvataggio 2

--- Tombola/Program.cs
+++ Tombola/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Configuration;
 using System.Threading;
 
 namespace Tombola
@@ -29,20 +30,52 @@ namespace Tombola
             bool[] b = new bool[90];
 
             //generazione tabellone
-            Console.SetCursorPosition(22, 1);
+            Console.SetCursorPosition(22, 1);           //posizionamento centrato per generazione tabellone
             Console.WriteLine("Tabellone: ");
             for(int i = 0; i < 9; i++)
             {
-                x = 13; //posizionamento tabellone
+                x = 13;                                 //posizionamento tabellone
                 for (int j = 2; j < 12; j++)
                 {
-                    Console.SetCursorPosition(x, y);
-                    Console.WriteLine(var);
-                    var++;
+                    Console.SetCursorPosition(x, y);    //posizionamento cursore per stampa
+                    Console.WriteLine(var);             //stampa variabile
+                    var++;                              //incremento variabile di stampa
                     x += 3;
                 }
                 y++;
             }
+
+            //generazione cartella 1
+            gcart1();
+
+            //generazione cartella 2
+            gcart2();
+
+            //caricamento matrice della prima cartella
+            mcart1();
+
+            //caricamento matrice della seconda cartella
+            mcart2();
+
+            //estrazione dei numeri e controllo
+            for (int i = 0; i < 90; i++)
+            {
+                num = estrazione();                                 //estrazione numero attraverso funzione apposita
+                x = Cx();                                           //calcolo delle x
+                y = Cy();                                           //calcolo delle y
+
+                //evidenziamento numero sul tabellone
+                Console.SetCursorPosition(x, y);                    //impostare posizione per evidenziare
+                Console.BackgroundColor = ConsoleColor.Green;       //impostazione colore di evidenziamento
+
+                //evidenziamento colore sulle cartelle attraverso funzioni apposita
+                contcart1();
+                contcart2();
+                Thread.Sleep(500);
+            }
+
+            //Funzioni {
+            //}
         }
     }
 }
