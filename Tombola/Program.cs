f4a963bd57bd4dc5ba96b8494dfb6cfe5760f5b9
Riccardo Camozzi <camozzi.riccardo06@gmail.com>
2023-01-10 15:01:19 +0100

    Salvataggio num1

--- Tombola/Program.cs
+++ Tombola/Program.cs
@@ -1,8 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Threading;
 
 namespace Tombola
 {
@@ -10,6 +7,42 @@ namespace Tombola
     {
         static void Main(string[] args)
         {
+            //variabile stampa tabellone
+            int var = 0;
+
+            //dichiarazione variabili
+            int num, x;
+            int y = 2;
+
+            //dichiarazione variabili cartelle per segnalazione tombola
+            int car1t = 0;
+            int car2t = 0;
+
+            //dichiarazione matrici per cartelle
+            int[,] car1 = new int[9, 3];
+            int[,] car2 = new int[9, 3];
+
+            //variabile random
+            Random r = new Random();
+
+            //dichiarazione di un array booleano
+            bool[] b = new bool[90];
+
+            //generazione tabellone
+            Console.SetCursorPosition(22, 1);
+            Console.WriteLine("Tabellone: ");
+            for(int i = 0; i < 9; i++)
+            {
+                x = 13; //posizionamento tabellone
+                for (int j = 2; j < 12; j++)
+                {
+                    Console.SetCursorPosition(x, y);
+                    Console.WriteLine(var);
+                    var++;
+                    x += 3;
+                }
+                y++;
+            }
         }
     }
 }
